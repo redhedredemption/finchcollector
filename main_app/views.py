3a32cc1521a1c39c62ef655e745f04033cfcd161
Molly Calhoun <mollyfcalhoun@gmail.com>
2024-05-30 16:16:40 -0700

    Part I: Initial Setup and Views

--- main_app/views.py
+++ main_app/views.py
@@ -1,3 +1,29 @@
 from django.shortcuts import render
 
+# Add this finches list below the imports
+finches = [
+  {'name': 'Lolo', 'breed': 'tabby', 'description': 'furry little demon', 'age': 3},
+  {'name': 'Sachi', 'breed': 'calico', 'description': 'gentle and loving', 'age': 2},
+]
+
+
 # Create your views here.
+
+# Define the home view
+def home(request):
+  # Include an .html file extension - unlike when rendering EJS templates
+  return render(request, 'home.html')
+
+
+# Define the about view
+def about(request):
+  # Include an .html file extension - unlike when rendering EJS templates
+  return render(request, 'about.html')
+
+# Add new view
+def finches_index(request):
+  # We pass data to a template very much like we did in Express!
+  return render(request, 'finches/index.html', {
+    'finches': finches
+  })
+
